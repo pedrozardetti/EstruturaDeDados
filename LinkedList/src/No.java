adb9b9cc707ff9738f5eee69a28ea574f7e1cad8
pedrozardetti <pedrozardetti1@gmail.com>
2024-09-19 20:00:21 -0300

    feat: create constructor for the node

---
+++ LinkedList/src/No.java
@@ -0,0 +1,9 @@
+public class No<Tipo> {
+    private Tipo elemento;
+    private No<Tipo> proximo;
+
+    public No(Tipo elemento) {
+        this.elemento = elemento;
+        this.proximo = null;
+    }
+}
