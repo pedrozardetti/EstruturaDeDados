1f37ef7664cd51ec11a1dc5d69977b8b90aa5154
pedrozardetti <pedrozardetti1@gmail.com>
2024-09-26 19:46:51 -0300

    feat: create linked list

--- LinkedList/src/Lista.java
+++ LinkedList/src/Lista.java
@@ -11,8 +11,10 @@ public class Lista {
 
         System.out.println(lista);
         System.out.println("Tamanho da lista = "  + lista.getTamanho());
-        System.out.println("Início da Lista = "  + lista.getInicio());
-        System.out.println("Fim da Lista = "  + lista.getFim());
+        System.out.println("Início da Lista = "  + lista.getInicio().getElemento());
+        System.out.println("Fim da Lista = "  + lista.getFim().getElemento());
+        
+        System.out.println("Elemento na posição 2 = "  + lista.get(2).getElemento());
 
     }
 }
